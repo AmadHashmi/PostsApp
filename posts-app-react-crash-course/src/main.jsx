a12c88d059ea509757971303d73c4ac8035f5ce3
Amad Ul Hassan <ah@indigilabs.com>
2024-12-25 12:26:40 +0100

    finished adding the single page for viewing the post
    finished adding the single page for viewing the post

--- posts-app-react-crash-course/src/main.jsx
+++ posts-app-react-crash-course/src/main.jsx
@@ -1,18 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import App from "./App";
+import Posts, { loader as postsLoader } from "./routes/Posts";
 import "./index.css";
-import NewPost from "./components/NewPost";
+import NewPost, { action as newPostAction } from "./routes/NewPost";
 import RootLayout from "./routes/RootLayout";
+import PostDetails, { loader as postDetailsLoader } from "./routes/PostDetails";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <RootLayout />,
     children: [
-      { path: "/", element: <App /> },
-      { path: "/create-post", element: <NewPost /> },
+      {
+        path: "/",
+        element: <Posts />,
+        loader: postsLoader,
+        children: [
+          { path: "/create-post", element: <NewPost />, action: newPostAction },
+          { path: "/:id", element: <PostDetails />, loader: postDetailsLoader },
+        ],
+      },
     ],
   },
 ]);
